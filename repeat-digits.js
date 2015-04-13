0fd46d47f5251ae48d39c8b495affdf12414081a
Mercedes Egido <meegido@gmail.com>
2015-04-13 19:30:51 +0200

    without indexOf method

--- repeat-digits.js
+++ repeat-digits.js
@@ -1,22 +1,18 @@
+
+var firstDigit = -1;
+
 function digitRepeat(number) {
-	for (i = 0; i < number.length; i++){ //itero
+	for (i = 0; i < number.length - 1; i++){
 		var digit = number[i];
 
-		if(number.indexOf(digit, i +1) !== -1){ //compruebo
-			return true
+		if(digit == number[i + 1]){
+			return true;
 		}
+		return false;
 	}
-	return false //valor por defecto (a menos que haya algún número repetido, lo compruebo dentro del for)
 }
-
-
-
-
-
-
 
 console.log(digitRepeat("199") );
 console.log(digitRepeat("34545") );
 console.log(digitRepeat("4444444443") );
 console.log(digitRepeat("138"));
-
